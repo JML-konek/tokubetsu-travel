13d42f48c3d93a592b8818c44fe4916a6071bddf
Gossip <132025922+gossipme@users.noreply.github.com>
2023-06-30 00:39:21 +0800

    add: deploy workflow

---
+++ travel-tour/src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import { FaSearchLocation } from 'react-icons/fa';
+
+import background from '/assets/ilocos-1.mp4';
+
+const Hero = () => {
+  return (
+    <div className="w-full h-screen relative">
+      <video
+        src={background}
+        autoPlay
+        loop
+        muted
+        className="w-full h-full object-cover"
+      ></video>
+
+      {/* OVERLAY */}
+      <div className="absolute w-full h-full top-0 left-0 bg-gray-900/20"></div>
+
+      {/* Content */}
+
+      <div className="absolute top-0 w-full h-full flex flex-col justify-center text-center p-4 font-poppins font-bold">
+        <h1 className="content">Lorem, ipsum dolor.</h1>
+        <h2 className="content">Lorem ipsum dolor sit, amet consectetur.</h2>
+        <form
+          className="flex justify-between items-center max-w-[700px] mx-auto w-full border p-1 rounded-full text-black
+         bg-green-200/90"
+        >
+          <div>
+            <input
+              className="bg-transparent w-[300px] sm:w-[400px] font-poppins focus:outline-none ml-2"
+              type="text"
+              placeholder="Search Destination        "
+            />
+          </div>
+          <div>
+            <button className=" text-blue-600">
+              <FaSearchLocation size={30} className="icon" />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ travel-tour/src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import { AiOutlineLogin } from 'react-icons/ai';
+import { AiOutlineClose } from 'react-icons/ai';
+import { SiYourtraveldottv } from 'react-icons/si';
+import { TiThMenu } from 'react-icons/ti';
+import { useState } from 'react';
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const slideNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <header className="flex w-full justify-between items-center h-20 px-5 font-semibold absolute z-10 text-white">
+      <div className="flex items-center gap-1">
+        <SiYourtraveldottv size={30} />
+        <h1>TRAVEL.</h1>
+      </div>
+      <ul className="hidden md:flex">
+        <li>Destination</li>
+        <li>Tour</li>
+        <li>Packages</li>
+        <li>Contacts</li>
+      </ul>
+      <div id="login" className="hidden md:flex">
+        <AiOutlineLogin size={30} />
+      </div>
+
+      {/* Menu */}
+
+      <div onClick={slideNav} className="md:hidden z-10">
+        {nav ? (
+          <AiOutlineClose size={30} className="text-black" />
+        ) : (
+          <TiThMenu size={30} />
+        )}
+      </div>
+
+      {/* // Mobile // */}
+
+      <div
+        onClick={slideNav}
+        className={
+          nav
+            ? 'absolute left-0 top-0 w-full bg-gray-300 rounded-sm px-4 py-7 flex flex-col duration-500 md:hidden'
+            : 'absolute left-[-100%] '
+        }
+      >
+        <ul className=" ">
+          <li>Destination</li>
+          <li>Tour</li>
+          <li>Packages</li>
+          <li>Contacts</li>
+          <div className="flex flex-col gap-2">
+            <button className="mobile-btn">Login</button>
+            <button className="mobile-btn">Account</button>
+          </div>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
